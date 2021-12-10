365ef261d55a4ea17e5b74ea79e42e05d7272549
Colin Bell <colin@ps116.org.uk>
2021-12-10 05:29:25 +0000

    2021/10 done. Committed as coded (needs tidying up)

--- src/aoc2021/day10.py
+++ src/aoc2021/day10.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from statistics import median
 
 
 class Run_2021_10(DayBase):
@@ -7,12 +8,56 @@ class Run_2021_10(DayBase):
     DAY = "10"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+match = {"}": "{", "]": "[", ")": "(", ">": "<"}
+closing = "}])>"
+points = {"}": 1197, "]": 57, ")": 3, ">": 25137}
+comp_score = {"(": 1, "[": 2, "{": 3, "<": 4}
+
+
+def part_a_score(line):
+    score = 0
+    cache = ""
+    for c in line:
+        if c in closing:
+            if match[c] == cache[-1]:
+                cache = cache[:-1]
+            else:
+                score = points[c]
+                break
+        else:
+            cache += c
+    if score:
+        return (score, None)
+    else:
+        score = 0
+        while cache:
+            c = cache[-1]
+            cache = cache[:-1]
+            score = score * 5 + comp_score[c]
+        return (0, score)
+
+
+def part_a(input, part_b=False):
+
+    score = 0
+    part_b_scores = []
+    for line in input_generator(input):
+        line_score, part_b_score = part_a_score(line)
+        if not part_b:
+            score += line_score
+        else:
+            if part_b_score is not None:
+                part_b_scores.append(part_b_score)
+    if not part_b:
+        return score
+    else:
+        return median(part_b_scores)
+
+    return score
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, part_b=True)
 
 
 if __name__ == "__main__":
--- tests/func/aoc2021/test_day10.py
+++ tests/func/aoc2021/test_day10.py
@@ -7,15 +7,13 @@ class Test_2021_10:
         self.day = Run_2021_10()
 
     def test_bringup_a(self):
-        pass
+        assert self.day.run_part("a", "test1") == 26397
 
     def test_bringup_b(self):
-        pass
+        assert self.day.run_part("b", "test1") == 288957
 
-    @pytest.mark.xfail
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 392043
 
-    @pytest.mark.xfail
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 1605968119
