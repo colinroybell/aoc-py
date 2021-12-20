0616bd0cf2d685f1d180b608144d6217049cb747
Colin Bell <colin@ps116.org.uk>
2021-12-20 13:37:21 +0000

    2015/07 done

--- src/aoc2015/day07.py
+++ src/aoc2015/day07.py
@@ -1,19 +1,77 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
-
+import re
 
 class Run_2015_07(DayBase):
     YEAR = "2015"
     DAY = "07"
 
+class Wire:
+    def __init__(self, id, input, circuit):
+        self.id = id
+        self.value = None
+        self.input = input
+        self.circuit = circuit
+
+    def get_value(self):
+        if self.value:
+            return self.value
+        else:
+            words = self.input.split(' ')
+            if len(words) == 1:
+                self.value = self.circuit.get_value(words[0])    
+            elif words[1] == 'AND':
+                self.value = self.circuit.get_value(words[0]) & self.circuit.get_value(words[2])
+            elif words[1] == 'OR':
+                self.value = self.circuit.get_value(words[0]) | self.circuit.get_value(words[2])
+            elif words[1] == 'LSHIFT':
+                self.value = (self.circuit.get_value(words[0]) << int(words[2])) & 65535
+            elif words[1] == 'RSHIFT':
+                self.value = (self.circuit.get_value(words[0]) >> int(words[2])) & 65535    
+            elif words[0] == 'NOT':
+                self.value = 65535 - self.circuit.get_value(words[1])
+        return self.value            
+
+    def reset(self):
+        self.value = None
+
+    def set_value(self, value):
+        self.value = value    
+
+class Circuit:
+    def __init__(self, input):
+        self.wires = {}
+        split_re = re.compile(r'(.+)\s->\s(.+)')
+        for line in input_generator(input):
+            m = split_re.match(line)
+            assert m 
+            self.wires[m.group(2)] = Wire(m.group(2),m.group(1),self)
+
+    def get_value(self, id):
+        if id.isnumeric():
+            return int(id)
+        else:    
+            return self.wires[id].get_value()       
+
+    def reset(self):
+        for _, w in self.wires.items():
+            w.reset()
+
+    def set_value(self, id, value):
+        self.wires[id].set_value(value)        
+
 
 def part_a(input):
-    assert 0, "not implemented"
+    circuit = Circuit(input)
+    return circuit.get_value('a')
 
 
 def part_b(input):
-    assert 0, "not implemented"
-
+    circuit = Circuit(input)
+    value = circuit.get_value('a')
+    circuit.reset()
+    circuit.set_value('b',value)
+    return circuit.get_value('a')
 
 if __name__ == "__main__":
     Run_2015_07().run_cmdline()
--- tests/func/aoc2015/test_day07.py
+++ tests/func/aoc2015/test_day07.py
@@ -1,5 +1,5 @@
 import pytest
-from aoc2015.day07 import Run_2015_07
+from aoc2015.day07 import Run_2015_07, Circuit
 
 
 class Test_2015_07:
@@ -7,15 +7,24 @@ class Test_2015_07:
         self.day = Run_2015_07()
 
     def test_bringup_a(self):
-        pass
+        circuit = Circuit(self.day.input_filename("a","test1"))
+        assert circuit.get_value('d') == 72
+        assert circuit.get_value('e') == 507
+        assert circuit.get_value('f') == 492
+        assert circuit.get_value('g') == 114
+        assert circuit.get_value('h') == 65412
+        assert circuit.get_value('i') == 65079
+        assert circuit.get_value('x') == 123
+        assert circuit.get_value('y') == 456
+
 
     def test_bringup_b(self):
         pass
 
     @pytest.mark.xfail
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 16076
 
     @pytest.mark.xfail
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 2797
