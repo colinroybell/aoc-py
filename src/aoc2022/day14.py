2a6a4836abe4dadaf3a34aeca020bb173a664325
Colin Bell <colin@ps116.org.uk>
2023-04-12 17:14:21 +0100

    Black reformatting

--- src/aoc2022/day14.py
+++ src/aoc2022/day14.py
@@ -3,12 +3,13 @@ from utils.data_input import input_generator
 from utils.vec_2d import Vec2d
 from utils.grid_2d import Grid2d
 
+
 class Run_2022_14(DayBase):
     YEAR = "2022"
     DAY = "14"
 
 
-def part_a(input, part_b = False):
+def part_a(input, part_b=False):
     part_a = not part_b
     grid = Grid2d()
     min_x = 500
@@ -17,72 +18,72 @@ def part_a(input, part_b = False):
     max_y = 0
 
     for line in input_generator(input):
-        words = line.split('->')
+        words = line.split("->")
         last_coord = None
         for word in words:
-            fields = word.split(',')
-            coord = Vec2d(int(fields[0]),int(fields[1]))
+            fields = word.split(",")
+            coord = Vec2d(int(fields[0]), int(fields[1]))
             if last_coord:
                 if last_coord.x == coord.x:
                     x = last_coord.x
                     ys = [last_coord.y, coord.y]
-                    min_x = min(min_x,x)
-                    max_x = max(max_x,x)
-                    min_y = min(min_y,min(ys))
-                    max_y = max(max_y,max(ys))
-                    for y in range(min(ys),max(ys)+1):
-                        grid.set(Vec2d(x,y),'#')
+                    min_x = min(min_x, x)
+                    max_x = max(max_x, x)
+                    min_y = min(min_y, min(ys))
+                    max_y = max(max_y, max(ys))
+                    for y in range(min(ys), max(ys) + 1):
+                        grid.set(Vec2d(x, y), "#")
                 elif last_coord.y == coord.y:
                     y = last_coord.y
                     xs = [last_coord.x, coord.x]
-                    min_y = min(min_y,y)
-                    max_y = max(max_y,y)
-                    min_x = min(min_x,min(xs))
-                    max_x = max(max_x,max(xs))
-                    for x in range(min(xs),max(xs)+1):
-                        grid.set(Vec2d(x,y),'#')
+                    min_y = min(min_y, y)
+                    max_y = max(max_y, y)
+                    min_x = min(min_x, min(xs))
+                    max_x = max(max_x, max(xs))
+                    for x in range(min(xs), max(xs) + 1):
+                        grid.set(Vec2d(x, y), "#")
                 else:
                     assert 0
 
             last_coord = coord
 
     if part_b:
-        for x in range(500-(max_y+5),500+(max_y+5)):
-            grid.set(Vec2d(x,max_y+2),'#')
+        for x in range(500 - (max_y + 5), 500 + (max_y + 5)):
+            grid.set(Vec2d(x, max_y + 2), "#")
 
-        min_x = min(min_x,500-(max_y+5))
-        max_x = max(max_x,500+(max_y+5))
+        min_x = min(min_x, 500 - (max_y + 5))
+        max_x = max(max_x, 500 + (max_y + 5))
         max_y += 2
 
-
-    #print(grid.to_string_as_characters(max_x+1,max_y+1,min_x,min_y))
+    # print(grid.to_string_as_characters(max_x+1,max_y+1,min_x,min_y))
     count = 0
     done = False
     while not done:
-        pos = Vec2d(500,0)
+        pos = Vec2d(500, 0)
         placed = False
         while pos.y < max_y:
-            candidates = [pos + Vec2d(0,1), pos + Vec2d(-1,1), pos + Vec2d(1,1)]
+            candidates = [pos + Vec2d(0, 1), pos + Vec2d(-1, 1), pos + Vec2d(1, 1)]
             moved = False
             for candidate in candidates:
                 if not grid.get(candidate):
-                    #print("dropped to {}".format(candidate))
+                    # print("dropped to {}".format(candidate))
                     pos = candidate
                     moved = True
                     break
             if not moved:
-                grid.set(pos,'o')
+                grid.set(pos, "o")
                 count += 1
                 placed = True
-                #print(count)
-                #print(grid.to_string_as_characters(max_x+1,max_y+1,min_x,min_y))
+                # print(count)
+                # print(grid.to_string_as_characters(max_x+1,max_y+1,min_x,min_y))
                 break
-        if part_a and not placed or part_b and pos == Vec2d(500,0):
+        if part_a and not placed or part_b and pos == Vec2d(500, 0):
             done = True
     return count
 
+
 def part_b(input):
-    return part_a(input,part_b=True)
+    return part_a(input, part_b=True)
 
 
 if __name__ == "__main__":
--- src/aoc2022/day19.py
+++ src/aoc2022/day19.py
@@ -6,10 +6,21 @@ class Run_2022_19(DayBase):
     YEAR = "2022"
     DAY = "19"
 
+
 # Create a state space hash as we don't care about resources over the maximum required to build something.
 
+
 class State:
-    def __init__(self, id, costs, time = 0, robots = [1,0,0,0], resources=[0,0,0,0], prog_min = 0, prog_max = 1):
+    def __init__(
+        self,
+        id,
+        costs,
+        time=0,
+        robots=[1, 0, 0, 0],
+        resources=[0, 0, 0, 0],
+        prog_min=0,
+        prog_max=1,
+    ):
         self.id = id
         self.costs = costs
         self.time = time
@@ -20,13 +31,22 @@ class State:
         self.robots_max = [max([costs[i][j] for i in range(4)]) for j in range(4)]
 
     def __str__(self):
-        return "id {} time {} robots {} resources {} progress {},{}".format(self.id, self.time, self.robots, self.resources,self.prog_min,self.prog_max)
+        return "id {} time {} robots {} resources {} progress {},{}".format(
+            self.id,
+            self.time,
+            self.robots,
+            self.resources,
+            self.prog_min,
+            self.prog_max,
+        )
 
     def copy(self):
-        return State(self.id, self.costs, self.time, self.robots.copy(), self.resources.copy())
+        return State(
+            self.id, self.costs, self.time, self.robots.copy(), self.resources.copy()
+        )
 
     def advance(self):
-        #print("From {}".format(self))
+        # print("From {}".format(self))
         next_step = []
         none = self.copy()
         none.time += 1
@@ -34,24 +54,28 @@ class State:
         for i in range(4):
             none.resources[i] += none.robots[i]
         for i in range(4):
-            if i < 3 and self.robots[i]==self.robots_max[i]:
+            if i < 3 and self.robots[i] == self.robots_max[i]:
                 continue
             if none.time == 24:
                 continue
-            if none.time >= 23 and (i == 0 or i==2):
+            if none.time >= 23 and (i == 0 or i == 2):
                 continue
-            if none.time >= 22 and i==1:
+            if none.time >= 22 and i == 1:
                 continue
             if all([self.resources[j] >= self.costs[i][j] for j in range(4)]):
                 robot_build = none.copy()
-                robot_build.robots[i] +=1
+                robot_build.robots[i] += 1
                 for j in range(4):
                     robot_build.resources[j] -= robot_build.costs[i][j]
                 next_step.append(robot_build)
         for i in range(len(next_step)):
-            next_step[i].prog_min = self.prog_min + i*(self.prog_max-self.prog_min)/len(next_step)
-            next_step[i].prog_max = self.prog_min + (i+1)*(self.prog_max-self.prog_min)/len(next_step)
-        #next_step.reverse()
+            next_step[i].prog_min = self.prog_min + i * (
+                self.prog_max - self.prog_min
+            ) / len(next_step)
+            next_step[i].prog_max = self.prog_min + (i + 1) * (
+                self.prog_max - self.prog_min
+            ) / len(next_step)
+        # next_step.reverse()
         return next_step
 
 
@@ -62,12 +86,17 @@ def part_a(input):
         words = line.split()
         id = int(words[1][:-1])
         ore_ore = int(words[6])
-        clay_ore =int(words[12])
+        clay_ore = int(words[12])
         ob_ore = int(words[18])
         ob_clay = int(words[21])
         ge_ore = int(words[27])
         ge_ob = int(words[30])
-        costs = [[ore_ore,0,0,0],[clay_ore,0,0,0],[ob_ore,ob_clay,0,0],[ge_ore,0,ge_ob,0]]
+        costs = [
+            [ore_ore, 0, 0, 0],
+            [clay_ore, 0, 0, 0],
+            [ob_ore, ob_clay, 0, 0],
+            [ge_ore, 0, ge_ob, 0],
+        ]
         initial_state = State(id, costs)
         state_queue.append(initial_state)
 
@@ -76,20 +105,20 @@ def part_a(input):
         while state_queue and count < 10000000:
             state = state_queue.pop()
             if state.time == 24:
-                count +=1
+                count += 1
                 geodes = state.resources[3]
-                improved = (geodes>best)
-                best = max(best,geodes)
-                if (count %100000 == 0 or improved):
-                    print("{}: {} done with {} best {}".format(count,state,geodes,best))
+                improved = geodes > best
+                best = max(best, geodes)
+                if count % 100000 == 0 or improved:
+                    print(
+                        "{}: {} done with {} best {}".format(count, state, geodes, best)
+                    )
             else:
                 advances = state.advance()
                 state_queue.extend(advances)
 
         total += id * best
     return total
-
-
 
 
 def part_b(input):
