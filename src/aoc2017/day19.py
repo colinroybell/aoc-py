574d693bab627c5d581d65c15e9bbbe2b7f91bcd
Colin Bell <colin@ps116.org.uk>
2024-01-29 16:57:42 +0000

    Ported 2017/19

--- src/aoc2017/day19.py
+++ src/aoc2017/day19.py
@@ -7,12 +7,73 @@ class Run_2017_19(DayBase):
     DAY = "19"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def part_a(input, part_b=False):
+    part_a = not part_b
+    grid = []
+    for line in input_generator(input):
+        grid.append(line)
+
+    def getItem(pos):
+        nonlocal grid
+        (x, y) = pos
+        print("trying: {}".format(pos))
+        if y < 0 or y >= len(grid):
+            return " "
+        gridLine = grid[y]
+        if x < 0 or x >= len(gridLine):
+            return " "
+        return gridLine[x]
+
+    def advance(pos, dir_):
+        return (pos[0] + dir_[0], pos[1] + dir_[1])
+
+    def left(dir_):
+        return (dir_[1], -dir_[0])
+
+    def right(dir_):
+        return (-dir_[1], dir_[0])
+
+    for i in range(len(grid[0])):
+        if getItem((i, 0)) != " ":
+            pos = (i, 0)
+
+    dir_ = (0, 1)
+    output = ""
+    steps = 1
+    while True:
+        print("At {}".format(pos, dir_))
+        posS = advance(pos, dir_)
+        itemS = getItem(posS)
+        posL = advance(pos, left(dir_))
+        itemL = getItem(posL)
+        posR = advance(pos, right(dir_))
+        itemR = getItem(posR)
+        if itemS != " ":
+            item = itemS
+            pos = posS
+        elif itemL != " ":
+            item = itemL
+            dir_ = left(dir_)
+            pos = posL
+        elif itemR != " ":
+            item = itemR
+            dir_ = right(dir_)
+            pos = posR
+        else:
+            break
+
+        steps += 1
+        if ord(item) > 64 and ord(item) <= 90:
+            output += item
+            print("Found {}".format(item))
+    if part_a:
+        return output
+    else:
+        return steps
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, True)
 
 
 if __name__ == "__main__":
--- tests/func/aoc2017/test_day18.py
+++ tests/func/aoc2017/test_day18.py
@@ -15,6 +15,5 @@ class Test_2017_18:
     def test_regression_a(self):
         assert self.day.run_part("a") == 2951
 
-    @pytest.mark.xfail
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 7366
