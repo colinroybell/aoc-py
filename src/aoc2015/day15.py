d1662556d854b554e9e0b911db6384d4aa1f4710
colin <colin@ps116.org.uk>
2024-10-31 07:57:56 +0000

    2015/15

--- src/aoc2015/day15.py
+++ src/aoc2015/day15.py
@@ -7,12 +7,56 @@ class Run_2015_15(DayBase):
     DAY = "15"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def recurse(attrs, count, counts, score_left, part_b):
+    if count == len(attrs) - 1:
+        counts.append(score_left)
+        prod = 1
+        amounts = []
+        for i in range(5):
+            amount = 0
+
+            for j in range(len(attrs)):
+                amount += counts[j] * attrs[j][i]
+            if amount < 0:
+                amount = 0
+            if i == 4:
+                print(amount, part_b)
+                if not part_b or part_b and amount == 500:
+                    amount = 1
+                else:
+                    amount = 0
+            prod *= amount
+            amounts.append(amount)
+        print(counts, amounts, prod)
+        return prod
+    else:
+        best = 0
+        for s in range(score_left + 1):
+            new_counts = counts.copy()
+            new_counts.append(s)
+            prod = recurse(attrs, count + 1, new_counts, score_left - s, part_b)
+            best = max(best, prod)
+        return best
+
+
+def part_a(input, part_b=False):
+    attrs = []
+    for line in input_generator(input):
+        f1 = line.split(":")
+        f2 = f1[1].split(",")
+        attr_line = []
+        for i in range(5):
+            f3 = f2[i].split(" ")
+            print(f3)
+            attr_line.append(int(f3[2]))
+        attrs.append(attr_line)
+    print(attrs)
+
+    return recurse(attrs, 0, [], 100, part_b)
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, part_b=True)
 
 
 if __name__ == "__main__":
--- tests/func/aoc2015/test_day15.py
+++ tests/func/aoc2015/test_day15.py
@@ -7,15 +7,13 @@ class Test_2015_15:
         self.day = Run_2015_15()
 
     def test_bringup_a(self):
-        pass
+        assert self.day.run_part("a", "test1") == 62842880
 
     def test_bringup_b(self):
-        pass
+        assert self.day.run_part("b", "test1") == 57600000
 
-    @pytest.mark.xfail
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 13882464
 
-    @pytest.mark.xfail
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 11171160
