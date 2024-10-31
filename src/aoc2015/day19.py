d80814feb5d63927bf99effade43c13e24c51c89
colin <colin@ps116.org.uk>
2024-10-31 19:07:55 +0000

    2015/19a plus notes on how to do b

--- src/aoc2015/day19.py
+++ src/aoc2015/day19.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2015_19(DayBase):
@@ -7,13 +8,64 @@ class Run_2015_19(DayBase):
     DAY = "19"
 
 
+def replacement(string, transitions):
+    outputs = set()
+    for (t0, t1) in transitions:
+        length = len(t0)
+        for m in re.finditer(t0, string):
+            new_string = string[: m.start()] + t1 + string[m.end() :]
+            outputs.add(new_string)
+    return len(outputs)
+
+
+def backward_steps(inputs, transitions):
+    outputs = set()
+    for (t0, t1) in transitions:
+        length = len(t1)
+        for string in inputs:
+            for m in re.finditer(t1, string):
+                new_string = string[: m.start()] + t0 + string[m.end() :]
+                outputs.add(new_string)
+    return outputs
+
+
 def part_a(input):
-    assert 0, "not implemented"
+    transitions = []
+    transitions_collection = True
+    for line in input_generator(input):
+        if transitions_collection:
+            if line == "":
+                transitions_collection = False
+            else:
+                fields = line.split(" => ")
+                transitions.append((fields[0], fields[1]))
+        else:
+            return replacement(line, transitions)
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    transitions = []
+    transitions_collection = True
+    for line in input_generator(input):
+        if transitions_collection:
+            if line == "":
+                transitions_collection = False
+            else:
+                fields = line.split(" => ")
+                transitions.append((fields[0], fields[1]))
+        else:
+            molecules = set()
+            molecules.add(line)
+            step = 0
+            while "e" not in molecules:
+                molecules = backward_steps(molecules, transitions)
+                step += 1
+                print(step, len(molecules))
+            return step
 
 
 if __name__ == "__main__":
     Run_2015_19().run_cmdline()
+
+# Idea: at top level work out a list of first steps. Take the best one, come up with a new list below. Backtrack and ensure we block out rejected ones. Also never do an e one unless that's everything.
+# Parsing in such a way that we break into the elements first might be nice, but not essential.
--- tests/func/aoc2015/test_day19.py
+++ tests/func/aoc2015/test_day19.py
@@ -6,16 +6,21 @@ class Test_2015_19:
     def setup_class(self):
         self.day = Run_2015_19()
 
-    def test_bringup_a(self):
-        pass
+    def test_bringup_a_1(self):
+        assert self.day.run_part("a", "test1") == 4
 
-    def test_bringup_b(self):
-        pass
+    def test_bringup_a_2(self):
+        assert self.day.run_part("a", "test2") == 7
 
-    @pytest.mark.xfail
+    def test_bringup_b_1(self):
+        assert self.day.run_part("b", "test1") == 3
+
+    def test_bringup_b_2(self):
+        assert self.day.run_part("b", "test2") == 6
+
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 535
 
-    @pytest.mark.xfail
+    @pytest.mark.skip
     def test_regression_b(self):
         assert self.day.run_part("b") == 0
