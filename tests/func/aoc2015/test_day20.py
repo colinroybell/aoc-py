bff244c6d6acb37673da6158bd8a0588f1eef5ca
colin <colin@ps116.org.uk>
2024-11-01 21:34:13 +0000

    2015/20

--- tests/func/aoc2015/test_day20.py
+++ tests/func/aoc2015/test_day20.py
@@ -6,16 +6,8 @@ class Test_2015_20:
     def setup_class(self):
         self.day = Run_2015_20()
 
-    def test_bringup_a(self):
-        pass
-
-    def test_bringup_b(self):
-        pass
-
-    @pytest.mark.xfail
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 665280
 
-    @pytest.mark.xfail
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 705600
