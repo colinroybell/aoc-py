5be17f954b0679d0d35821bc6eff9b20cee86717
colin <colin@ps116.org.uk>
2024-11-12 07:41:13 +0000

    EC2024/06

--- src/ec2024/day06.py
+++ src/ec2024/day06.py
@@ -8,16 +8,50 @@ class Run_2024_06(DayBase):
     PREFIX = "ec"
 
 
-def part_1(input):
-    assert 0, "not implemented"
+def recurse(part, nodes, node, count, string):
+    fruits = {}
+    if node == "@":
+        fruits[count] = string
+    elif node == "ANT" or node == "BUG":
+        pass
+    elif node in nodes:
+        for s in nodes[node]:
+            if part == 1:
+                new_string = string + s
+            else:
+                new_string = string + s[0]
+            s_fruits = recurse(part, nodes, s, count + 1, new_string)
+            for s_count, s_string in s_fruits.items():
+                if s_count not in fruits:
+                    fruits[s_count] = s_string
+                else:
+                    fruits[s_count] = "DUP"
+    return fruits
+
+
+def part_1(input, part=1):
+    nodes = {}
+    for line in input_generator(input):
+        fields = line.split(":")
+        subnodes = fields[1].split(",")
+        nodes[fields[0]] = subnodes
+    if part == 1:
+        base_string = "RR"
+    else:
+        base_string = "R"
+    fruits = recurse(part, nodes, "RR", 1, base_string)
+    for count, string in fruits.items():
+        if string != "DUP":
+            return string
+    return "none"
 
 
 def part_2(input):
-    assert 0, "not implemented"
+    return part_1(input, part=2)
 
 
 def part_3(input):
-    assert 0, "not implemented"
+    return part_1(input, part=3)
 
 
 if __name__ == "__main__":
--- tests/func/ec2024/test_day06.py
+++ tests/func/ec2024/test_day06.py
@@ -7,22 +7,13 @@ class Test_2024_06:
         self.day = Run_2024_06()
 
     def test_bringup_1(self):
-        pass
+        assert self.day.run_part("1", "test1") == "RRB@"
 
-    def test_bringup_2(self):
-        pass
-
-    def test_bringup_3(self):
-        pass
-
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == "RRXKQXBSGRMW@"
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == "RKRMFGBPJB@"
 
-    @pytest.mark.skip
     def test_regression_3(self):
-        assert self.day.run_part("3") == 0
+        assert self.day.run_part("3") == "RRPMJQRFPDTZ@"
