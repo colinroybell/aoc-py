08e16ece7edb44d4089e6025af386780c5312511
colin <colin@ps116.org.uk>
2024-11-16 07:51:12 +0000

    EC2024/09

--- src/ec2024/day09.py
+++ src/ec2024/day09.py
@@ -7,16 +7,69 @@ class Run_2024_09(DayBase):
     DAY = "09"
     PREFIX = "ec"
 
+# TODO: refactor and tidy up
 
 def part_1(input):
-    assert 0, "not implemented"
+    offsets = [0, 1, 2, 1, 2, 1, 2, 3, 4, 3]
+    total = 0
+    for line in input_generator(input):
+        target = int(line)
+        beetles = (target // 10) + offsets[target % 10]
+        print(beetles)
+        total += beetles
+    return total
 
 
 def part_2(input):
-    assert 0, "not implemented"
+    stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30]
+    targets = [int(line) for line in input_generator(input)]
+    maximum = max(targets)
+    counts = [0 for _ in range(maximum + 1)]
+    for b in range(1, maximum + 1):
+        best = None
+        for s in stamps:
+            if s <= b:
+                c = counts[b - s] + 1
+                if best == None or c < best:
+                    best = c
+        print(b, c)
+        counts[b] = best
+    return sum(counts[t] for t in targets)
 
 
 def part_3(input):
+    stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101]
+    targets = [int(line) for line in input_generator(input)]
+    maximum = max(targets)
+    diff = 100
+    length = maximum // 2 + diff // 2
+    counts = [0 for _ in range(length + 1)]
+    for b in range(1, length + 1):
+        best = None
+        for s in stamps:
+            if s <= b:
+                c = counts[b - s] + 1
+                if b == 41:
+                    print("t", s, c)
+                if best == None or c < best:
+                    best = c
+        # print(b,c)
+        counts[b] = best
+    total = 0
+    for t in targets:
+        best = None
+        hs = t // 2
+        hl = t - hs
+        while hl - hs <= 100:
+            score = counts[hs] + counts[hl]
+            if best == None or score < best:
+                print(hl, hs, score)
+                best = score
+            hs -= 1
+            hl += 1
+        total += best
+    return total
+
     assert 0, "not implemented"
 
 
--- tests/func/ec2024/test_day09.py
+++ tests/func/ec2024/test_day09.py
@@ -7,22 +7,19 @@ class Test_2024_09:
         self.day = Run_2024_09()
 
     def test_bringup_1(self):
-        pass
+        assert self.day.run_part("1", "test1") == 10
 
     def test_bringup_2(self):
-        pass
+        assert self.day.run_part("2", "test2") == 10
 
     def test_bringup_3(self):
-        pass
+        assert self.day.run_part("3", "test3") == 10449
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 12063
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 5015
 
-    @pytest.mark.skip
     def test_regression_3(self):
-        assert self.day.run_part("3") == 0
+        assert self.day.run_part("3") == 152079
