da81f1fc4937b335c35e1c502b79af08a74c47d8
colin <colin@ps116.org.uk>
2024-11-30 03:02:31 +0000

    EC2024/20 part 1

--- src/ec2024/day20.py
+++ src/ec2024/day20.py
@@ -1,5 +1,7 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from utils.grid_2d import Grid2d
+from utils.vec_2d import Vec2d
 
 
 class Run_2024_20(DayBase):
@@ -8,8 +10,35 @@ class Run_2024_20(DayBase):
     PREFIX = "ec"
 
 
+# Goal is to reach one of the points in row 20 with minimum .
 def part_1(input):
-    assert 0, "not implemented"
+    grid = Grid2d()
+    width, height = grid.read_from_file_strings(input)
+    assert grid.get(Vec2d(15, 0)) == "S"
+    starts = [(15, 1100)]
+    # score is drop from
+    for y in range(2, 22, 2):
+        new_starts = []
+        for x in range(width):
+            c = grid.get(Vec2d(x, y))
+            if c == "-" or c == "+":
+                best = None
+                for xx, s in starts:
+                    score = s - 2 * (abs(x - xx) + 2)
+                    if best == None or score > best:
+                        best = score
+                if c == "-":
+                    best = best - 1
+                else:
+                    best = best + 2
+                new_starts.append((x, best))
+        starts = new_starts
+        print(y, starts)
+    best = None
+    for _, s in starts:
+        if best == None or s > best:
+            best = s
+    return best
 
 
 def part_2(input):
--- tests/func/ec2024/test_day20.py
+++ tests/func/ec2024/test_day20.py
@@ -6,18 +6,24 @@ class Test_2024_20:
     def setup_class(self):
         self.day = Run_2024_20()
 
-    def test_bringup_1(self):
-        pass
-
-    def test_bringup_2(self):
-        pass
-
-    def test_bringup_3(self):
-        pass
+    # We wrote a bespoke test for this one
+    # def test_bringup_1(self):
+    #    pass
 
     @pytest.mark.skip
+    def test_bringup_2_2(self):
+        assert self.day.run_part("2", "test2") == 24
+
+    @pytest.mark.skip
+    def test_bringup_2_3(self):
+        assert self.day.run_part("2", "test2") == 78
+
+    @pytest.mark.skip
+    def test_bringup_2_4(self):
+        assert self.day.run_part("2", "test2") == 206
+
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 1032
 
     @pytest.mark.skip
     def test_regression_2(self):
