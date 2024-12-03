f8ee0b5943fa2608a0a49a124cdae0af04671761
colin <colin@ps116.org.uk>
2024-12-03 06:55:05 +0000

    2024/03

--- src/aoc2024/day03.py
+++ src/aoc2024/day03.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2024_03(DayBase):
@@ -8,11 +9,33 @@ class Run_2024_03(DayBase):
 
 
 def part_a(input):
-    assert 0, "not implemented"
+    mulre = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+    total = 0
+    for line in input_generator(input):
+        finds = mulre.findall(line)
+        for f in finds:
+            print(f)
+            total += int(f[0]) * int(f[1])
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    mulre = re.compile(r"^mul\((\d{1,3}),(\d{1,3})\)")
+    total = 0
+    enabled = True
+    for line in input_generator(input):
+
+        for i in range(len(line)):
+            subline = line[i:]
+            if subline.startswith("do()"):
+                enabled = True
+            elif subline.startswith("don't()"):
+                enabled = False
+            elif enabled and subline.startswith("mul"):
+                m = mulre.match(subline)
+                if m:
+                    total += int(m.group(1)) * int(m.group(2))
+    return total
 
 
 if __name__ == "__main__":
--- tests/func/aoc2024/test_day03.py
+++ tests/func/aoc2024/test_day03.py
@@ -6,18 +6,14 @@ class Test_2024_03:
     def setup_class(self):
         self.day = Run_2024_03()
 
-    @pytest.mark.skip
     def test_bringup_a(self):
-        pass
+        assert self.day.run_part("a", "test1") == 161
 
-    @pytest.mark.skip
     def test_bringup_b(self):
-        pass
+        assert self.day.run_part("b", "test2") == 48
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 182780583
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 90772405
