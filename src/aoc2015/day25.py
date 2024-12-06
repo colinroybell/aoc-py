b81e0d89457d686a34622bc1ed5be0aaf4d7b0f2
colin <colin@ps116.org.uk>
2024-12-06 12:13:36 +0000

    2015/25 part 1

--- src/aoc2015/day25.py
+++ src/aoc2015/day25.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2015_25(DayBase):
@@ -7,8 +8,43 @@ class Run_2015_25(DayBase):
     DAY = "25"
 
 
+def locate(row, column):
+    round = row + column
+    round -= 2
+    return (round * (round + 1)) // 2 + column
+
+
+def lookup(row, column):
+
+    count = locate(row, column) - 1
+    base = 20151125
+    mul = 252533
+    mod = 33554393
+
+    print(count)
+
+    total = 1
+    while count:
+        if count % 2:
+            total = (total * mul) % mod
+        mul = (mul * mul) % mod
+        count //= 2
+
+    return (total * base) % mod
+
+
 def part_a(input):
-    assert 0, "not implemented"
+    parse_re = re.compile(r".*row (\d+).*column (\d+)")
+
+    m = parse_re.match(next(input_generator(input)))
+    row = int(m.group(1))
+    column = int(m.group(2))
+
+    for r in range(1, 7):
+        for c in range(1, 7):
+            print(r, c, lookup(r, c))
+
+    return lookup(row, column)
 
 
 def part_b(input):
--- tests/func/aoc2015/test_day25.py
+++ tests/func/aoc2015/test_day25.py
@@ -1,20 +1,23 @@
 import pytest
-from aoc2015.day25 import Run_2015_25
+from aoc2015.day25 import Run_2015_25, locate, lookup
 
 
 class Test_2015_25:
     def setup_class(self):
         self.day = Run_2015_25()
 
-    def test_bringup_a(self):
-        pass
+    def test_bringup_a_1(self):
+        assert locate(4, 2) == 12
+        assert locate(1, 5) == 15
 
-    def test_bringup_b(self):
-        pass
+    def test_bringup_a_2(self):
+        assert lookup(1, 1) == 20151125
+        assert lookup(1, 6) == 33511524
+        assert lookup(6, 1) == 33071741
+        assert lookup(6, 6) == 27995004
 
-    @pytest.mark.xfail
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 2650453
 
     @pytest.mark.xfail
     def test_regression_b(self):
