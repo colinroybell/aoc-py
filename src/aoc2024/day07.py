08a0a24b7116457421c3c22b35c82f126b372ee6
colin <colin@ps116.org.uk>
2024-12-07 10:12:20 +0000

    2024/07

--- src/aoc2024/day07.py
+++ src/aoc2024/day07.py
@@ -7,12 +7,34 @@ class Run_2024_07(DayBase):
     DAY = "07"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def recurse(part_b, target, list):
+    last = list[-1]
+    rest = list[0:-1]
+    if rest == []:
+        return target == last
+    if target % last == 0 and recurse(part_b, target // last, rest):
+        return True
+    if part_b and target > last and str(target).endswith(str(last)):
+        if recurse(part_b, int(str(target)[0 : -len(str(last))]), rest):
+            return True
+    if target > last and recurse(part_b, target - last, rest):
+        return True
+    return False
+
+
+def part_a(input, part_b=False):
+    total = 0
+    for line in input_generator(input):
+        target, nums = line.split(":")
+        target = int(target)
+        list = [int(n) for n in nums.split()]
+        if recurse(part_b, target, list):
+            total += target
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, part_b=True)
 
 
 if __name__ == "__main__":
--- tests/func/aoc2024/test_day07.py
+++ tests/func/aoc2024/test_day07.py
@@ -6,18 +6,14 @@ class Test_2024_07:
     def setup_class(self):
         self.day = Run_2024_07()
 
-    @pytest.mark.skip
     def test_bringup_a(self):
-        pass
+        assert self.day.run_part("a", "test1") == 3749
 
-    @pytest.mark.skip
     def test_bringup_b(self):
-        pass
+        assert self.day.run_part("b", "test1") == 11387
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 12839601725877
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 149956401519484
