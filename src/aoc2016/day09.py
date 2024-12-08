2d89301f7141d35269496da2d2a4b77eecc620f8
colin <colin@ps116.org.uk>
2024-12-08 15:38:57 +0000

    2016/09 part 1

--- src/aoc2016/day09.py
+++ src/aoc2016/day09.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2016_09(DayBase):
@@ -7,8 +8,45 @@ class Run_2016_09(DayBase):
     DAY = "09"
 
 
+def decompression_length(string):
+    match_re = re.compile(r"(.*?)\((\d+)x(\d+)\)(.*)")
+
+    output = 0
+    while 1:
+        m = match_re.match(string)
+        if not m:
+            output += len(string)
+            break
+        output += len(m.group(1))
+        length = int(m.group(2))
+        count = int(m.group(3))
+        rem = m.group(4)
+        output += length * count
+        string = rem[length:]
+    return output
+
+
+def apply_mults(length, mults):
+    pass
+
+
+def decompression_length2(string):
+    return 0
+    match_re = re.compile(r"(.*?)\((\d+)x(\d+)\)(.*)")
+    mults = []
+    output = 0
+    while 1:
+        m = match_re.match(string)
+        if not m:
+            output += apply_mults(len(string), mults)
+            break
+
+
 def part_a(input):
-    assert 0, "not implemented"
+    total = 0
+    for line in input_generator(input):
+        total += decompression_length(line)
+    return total
 
 
 def part_b(input):
--- tests/func/aoc2016/test_day09.py
+++ tests/func/aoc2016/test_day09.py
@@ -1,5 +1,5 @@
 import pytest
-from aoc2016.day09 import Run_2016_09
+from aoc2016.day09 import Run_2016_09, decompression_length, decompression_length2
 
 
 class Test_2016_09:
@@ -7,14 +7,26 @@ class Test_2016_09:
         self.day = Run_2016_09()
 
     def test_bringup_a(self):
-        pass
+        assert decompression_length("ADVENT") == 6
+        assert decompression_length("A(1x5)BC") == 7
+        assert decompression_length("(3x3)XYZ") == 9
+        assert decompression_length("A(2x2)BCD(2x2)EFG") == 11
+        assert decompression_length("(6x1)(1x3)A") == 6
+        assert decompression_length("X(8x2)(3x3)ABCY") == 18
 
     def test_bringup_b(self):
-        pass
+        assert decompression_length2("(3x3)XYZ") == 6
+        assert decompression_length2("X(8x2)(3x3)ABCY") == 6
+        assert decompression_length2("(27x12)(20x12)(13x14)(7x10)(1x12)A") == 6
+        assert (
+            decompression_length2(
+                "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+            )
+            == 6
+        )
 
-    @pytest.mark.xfail
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 150914
 
     @pytest.mark.xfail
     def test_regression_b(self):
