085486196f9f6874c03ac77094f67266e33e1f15
colin <colin@ps116.org.uk>
2024-12-11 19:51:31 +0000

    2024/11

--- src/aoc2024/day11.py
+++ src/aoc2024/day11.py
@@ -7,12 +7,40 @@ class Run_2024_11(DayBase):
     DAY = "11"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def next_n(n):
+    if n == 0:
+        return [1]
+    s = str(n)
+    if len(s) % 2 == 0:
+        h = len(s) // 2
+        return [int(s[:h]), int(s[h:])]
+    return [n * 2024]
+
+
+def recurse(n, blinks, cache):
+    if blinks == 0:
+        return 1
+    pair = (n, blinks)
+    if pair in cache:
+        return cache[pair]
+    nexts = next_n(n)
+    count = sum([recurse(num, blinks - 1, cache) for num in nexts])
+    cache[pair] = count
+    return count
+
+
+def part_a(input, blinks=25):
+    cache = {}
+    total = 0
+    for line in input_generator(input):
+        nums = [int(n) for n in line.split()]
+        for n in nums:
+            total += recurse(n, blinks, cache)
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, blinks=75)
 
 
 if __name__ == "__main__":
--- tests/func/aoc2024/test_day11.py
+++ tests/func/aoc2024/test_day11.py
@@ -1,23 +1,29 @@
 import pytest
-from aoc2024.day11 import Run_2024_11
+from aoc2024.day11 import Run_2024_11, next_n
 
 
 class Test_2024_11:
     def setup_class(self):
         self.day = Run_2024_11()
 
-    @pytest.mark.skip
-    def test_bringup_a(self):
-        pass
+    def test_bringup_next_n(self):
+        assert next_n(0) == [1]
+        assert next_n(1) == [2024]
+        assert next_n(10) == [1, 0]
+        assert next_n(99) == [9, 9]
+        assert next_n(999) == [2021976]
 
-    @pytest.mark.skip
+    def test_bringup_a_1(self):
+        assert self.day.run_part("a", ["125 17"], blinks=6) == 22
+
+    def test_bringup_a_2(self):
+        assert self.day.run_part("a", ["125 17"]) == 55312
+
     def test_bringup_b(self):
         pass
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 218079
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 259755538429618
