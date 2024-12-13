e32381a139289b6924a3d9d177f2fe1b3887a947
colin <colin@ps116.org.uk>
2024-12-13 21:11:01 +0000

    2024/13

--- src/aoc2024/day13.py
+++ src/aoc2024/day13.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2024_13(DayBase):
@@ -7,12 +8,39 @@ class Run_2024_13(DayBase):
     DAY = "13"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def part_a(input, part_b=False):
+    num_re = re.compile(r"(\d+).+?(\d+)")
+    state = 0
+    cost = 0
+    for line in input_generator(input):
+        if state < 3:
+            m = num_re.search(line)
+            assert m
+            x = int(m.group(1))
+            y = int(m.group(2))
+            if state == 0:
+                x1 = x
+                y1 = y
+            elif state == 1:
+                x2 = x
+                y2 = y
+            else:
+                if part_b:
+                    x += 10000000000000
+                    y += 10000000000000
+                delta = x1 * y2 - x2 * y1
+                a = y2 * x - x2 * y
+                b = -y1 * x + x1 * y
+                if a % delta == 0 and b % delta == 0:
+                    a //= delta
+                    b //= delta
+                    cost += a * 3 + b
+        state = (state + 1) % 4
+    return cost
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, part_b=True)
 
 
 if __name__ == "__main__":
