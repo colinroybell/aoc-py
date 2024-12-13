6d0127c2692c285f42a125616bf53b60e246adb4
colin <colin@ps116.org.uk>
2024-12-13 02:34:27 +0000

    2024/12

--- src/aoc2024/day12.py
+++ src/aoc2024/day12.py
@@ -1,5 +1,7 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from utils.grid_2d import Grid2d
+from utils.vec_2d import Vec2d
 
 
 class Run_2024_12(DayBase):
@@ -7,12 +9,68 @@ class Run_2024_12(DayBase):
     DAY = "12"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def perimeter_score(boundaries: set):
+    count = 0
+    while len(boundaries):
+        (pos, d) = boundaries.pop()
+        count += 1
+        if d in "UD":
+            cand = "LR"
+        else:
+            cand = "UD"
+        for c in cand:
+            p = pos.move(c)
+            n = (p, d)
+            while n in boundaries:
+                boundaries.remove(n)
+                p = p.move(c)
+                n = (p, d)
+    return count
+
+
+def part_a(input, part_b=False):
+    grid = Grid2d()
+    width, height = grid.read_from_file_strings(input)
+    score = 0
+    for y in range(height):
+        for x in range(width):
+            v = Vec2d(x, y)
+            c = grid.get(v)
+            if "*" not in c:
+                area = 0
+                perimeter = 0
+                boundaries = set()
+                cands = [v]
+                done = c + "*"
+                while cands:
+                    v = cands[0]
+                    cands.pop(0)
+                    val = grid.get(v)
+                    if val == c:
+                        area += 1
+                        for dir in "UDLR":
+                            a = v.move(dir)
+                            val_a = grid.get(a)
+                            if val_a == c:
+                                cands.append(a)
+                            elif val_a == done:
+                                pass
+                            else:
+                                perimeter += 1
+                                boundaries.add((v, dir))
+                        grid.set(v, done)
+                    elif val == done:
+                        pass
+                    else:
+                        assert 0
+                if part_b:
+                    perimeter = perimeter_score(boundaries)
+                score += area * perimeter
+    return score
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, part_b=True)
 
 
 if __name__ == "__main__":
--- tests/func/aoc2024/test_day12.py
+++ tests/func/aoc2024/test_day12.py
@@ -6,18 +6,29 @@ class Test_2024_12:
     def setup_class(self):
         self.day = Run_2024_12()
 
-    @pytest.mark.skip
-    def test_bringup_a(self):
-        pass
+    def test_bringup_a_1(self):
+        assert self.day.run_part("a", "test1") == 140
 
-    @pytest.mark.skip
-    def test_bringup_b(self):
-        pass
+    def test_bringup_a_2(self):
+        assert self.day.run_part("a", "test2") == 772
 
-    @pytest.mark.skip
+    def test_bringup_a_3(self):
+        assert self.day.run_part("a", "test3") == 1930
+
+    def test_bringup_b_1(self):
+        assert self.day.run_part("b", "test1") == 80
+
+    def test_bringup_b_2(self):
+        assert self.day.run_part("b", "test4") == 236
+
+    def test_bringup_b_3(self):
+        assert self.day.run_part("b", "test5") == 368
+
+    def test_bringup_b_4(self):
+        assert self.day.run_part("b", "test3") == 1206
+
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 1486324
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 898684
