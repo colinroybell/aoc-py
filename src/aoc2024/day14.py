1d998ecc0338c884f46a8d96b148bec6b73c7a3b
colin <colin@ps116.org.uk>
2024-12-16 07:43:46 +0000

    2024/14, 2024/15 (needs tidyup and test adding)

--- src/aoc2024/day14.py
+++ src/aoc2024/day14.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2024_14(DayBase):
@@ -7,12 +8,68 @@ class Run_2024_14(DayBase):
     DAY = "14"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def split_to_xy(split_re, string):
+    m = split_re.search(string)
+    print(string)
+    assert m
+    return (int(m.group(1)), int(m.group(2)))
 
 
-def part_b(input):
-    assert 0, "not implemented"
+def part_a(input, width=101, height=103):
+    split_re = re.compile(r"\=(.+),(.+)")
+    counts = [0, 0, 0, 0]
+    for line in input_generator(input):
+        pv = line.split()
+        (px, py) = split_to_xy(split_re, pv[0])
+        (vx, vy) = split_to_xy(split_re, pv[1])
+
+        x = (px + vx * 100) % width
+        y = (py + vy * 100) % height
+        if x < width // 2:
+            x_bucket = 0
+        elif x > width // 2:
+            x_bucket = 1
+        else:
+            x_bucket = None
+        if y < height // 2:
+            y_bucket = 0
+        elif y > height // 2:
+            y_bucket = 1
+        else:
+            y_bucket = None
+        if x_bucket != None and y_bucket != None:
+            counts[x_bucket + 2 * y_bucket] += 1
+        print(x, y, x_bucket, y_bucket, counts)
+    prod = 1
+    for c in counts:
+        prod *= c
+    return prod
+
+
+def part_b(input, width=101, height=103):
+    split_re = re.compile(r"\=(.+),(.+)")
+    counts = [0, 0, 0, 0]
+    robots = []
+    for line in input_generator(input):
+        pv = line.split()
+        (px, py) = split_to_xy(split_re, pv[0])
+        (vx, vy) = split_to_xy(split_re, pv[1])
+        robots.append([px, py, vx, vy])
+
+    min_sigma = None
+    min_time = None
+    for t in range(10000):
+        sigma = 0
+        for px, py, vx, vy in robots:
+            x = (px + vx * t) % width
+            y = (py + vy * t) % height
+            sigma += (x - width // 2) ** 2 + (y - height // 2) ** 2
+
+        if min_sigma == None or sigma < min_sigma:
+            min_sigma = sigma
+            min_time = t
+
+    return min_time
 
 
 if __name__ == "__main__":
--- src/aoc2024/day15.py
+++ src/aoc2024/day15.py
@@ -1,5 +1,7 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from utils.grid_2d import Grid2d
+from utils.vec_2d import Vec2d
 
 
 class Run_2024_15(DayBase):
@@ -7,12 +9,122 @@ class Run_2024_15(DayBase):
     DAY = "15"
 
 
+def can_advance(grid, pos, dir):
+    a = pos.move_y_flipped(dir)
+    c = grid.get(a)
+    move = False
+    if c == ".":
+        move = True
+    elif c == "#":
+        move = False
+    else:
+        _, move = can_advance(grid, a, dir)
+    if move:
+        print("move", grid.get(pos), dir, "to", a)
+        grid.set(a, grid.get(pos))
+        grid.set(pos, ".")
+        return a, True
+    else:
+        return pos, False
+
+
+def can_advance_iterative(grid, pos, dir):
+    done = set()
+    to_blank = []
+    to_set = []
+    to_consider = [pos]
+    while to_consider:
+        p = to_consider.pop(0)
+        if p in done:
+            continue
+        done.add(p)
+        a = p.move_y_flipped(dir)
+        c = grid.get(a)
+        if c == "#":
+            return pos, False
+        if c != ".":
+            to_consider.append(a)
+        if c == "[":
+            to_consider.append(a.move(">"))
+        if c == "]":
+            to_consider.append(a.move("<"))
+        to_blank.append(p)
+        to_set.append((a, grid.get(p)))
+    print(to_blank)
+    print(to_set)
+    for p in to_blank:
+        grid.set(p, ".")
+    for (p, v) in to_set:
+        grid.set(p, v)
+    return pos.move_y_flipped(dir), True
+
+
 def part_a(input):
-    assert 0, "not implemented"
+    grid = Grid2d()
+    generator = input_generator(input)
+    width, height = grid.read_from_file_strings_generator(generator, stop_at_blank=True)
+    for y in range(height):
+        for x in range(width):
+            v = Vec2d(x, y)
+            if grid.get(v) == "@":
+                guard = v
+
+    for line in generator:
+        for dir in line:
+            print("dir", dir)
+            guard, _ = can_advance_iterative(grid, guard, dir)
+
+    total = 0
+    for y in range(height):
+        for x in range(width):
+            if grid.get(Vec2d(x, y)) == "O":  #
+                total += y * 100 + x
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    grid = Grid2d()
+    grid_assemble = True
+
+    y = 0
+    for line in input_generator(input):
+        if grid_assemble:
+            if line == "":
+                grid_assemble = False
+                width = x
+                height = y
+            else:
+                x = 0
+                for c in line:
+                    p0 = Vec2d(x, y)
+                    p1 = Vec2d(x + 1, y)
+                    if c == "#":
+                        grid.set(p0, "#")
+                        grid.set(p1, "#")
+                    elif c == "O":
+                        grid.set(p0, "[")
+                        grid.set(p1, "]")
+                    elif c == ".":
+                        grid.set(p0, ".")
+                        grid.set(p1, ".")
+                    else:
+                        grid.set(p0, "@")
+                        grid.set(p1, ".")
+                        guard = p0
+                    x += 2
+                y += 1
+
+        else:
+            for dir in line:
+                print("dir", dir)
+                guard, _ = can_advance_iterative(grid, guard, dir)
+
+    total = 0
+    for y in range(height):
+        for x in range(width):
+            if grid.get(Vec2d(x, y)) == "[":
+                total += y * 100 + x
+    return total
 
 
 if __name__ == "__main__":
