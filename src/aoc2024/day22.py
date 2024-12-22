070eadc56e350a65c1f19dfcaafa2553f4f2d29f
colin <colin@ps116.org.uk>
2024-12-22 08:26:44 +0000

    2024/22

--- src/aoc2024/day22.py
+++ src/aoc2024/day22.py
@@ -7,12 +7,76 @@ class Run_2024_22(DayBase):
     DAY = "22"
 
 
+def next_number(n):
+    n = (n ^ (n * 64)) % 16777216
+    n = (n ^ (n // 32)) % 16777216
+    n = (n ^ (n * 2048)) % 16777216
+    return n
+
+
 def part_a(input):
-    assert 0, "not implemented"
+    total = 0
+    for line in input_generator(input):
+        n = int(line)
+        for _ in range(2000):
+            n = next_number(n)
+        total += n
+    return total
+
+
+def score(secrets, changes):
+    print(secrets, changes)
+    total = 0
+    for i, n in enumerate(secrets):
+        diffs = [None, None, None, None]
+        last = None
+        for s in range(2000):
+            n = next_number(n)
+            price = n % 10
+            for j in range(3):
+                diffs[j] = diffs[j + 1]
+            if s > 0:
+                diffs[3] = price - last
+            if 288 < s < 292:
+                print(s, diffs, changes)
+            last = price
+            if diffs == changes:
+                print("found {} at pos {} price {}".format(i, s, price))
+                total += price
+                break
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    secrets = []
+    points = {}
+
+    for line in input_generator(input):
+        n = int(line)
+
+        this_points = {}
+        diffs = [None, None, None, None]
+        for s in range(2000):
+            n = next_number(n)
+            price = n % 10
+            for j in range(3):
+                diffs[j] = diffs[j + 1]
+            if s > 0:
+                diffs[3] = price - last
+            last = price
+            t = tuple(diffs)
+            if s >= 4 and t not in this_points:
+                this_points[t] = price
+        for d, v in this_points.items():
+            if d not in points:
+                points[d] = 0
+            points[d] += v
+    max = 0
+    for d, v in points.items():
+        if v > max:
+            max = v
+
+    return max
 
 
 if __name__ == "__main__":
--- tests/func/aoc2024/test_day22.py
+++ tests/func/aoc2024/test_day22.py
@@ -1,23 +1,37 @@
 import pytest
-from aoc2024.day22 import Run_2024_22
+from aoc2024.day22 import Run_2024_22, next_number, score
 
 
 class Test_2024_22:
     def setup_class(self):
         self.day = Run_2024_22()
 
-    @pytest.mark.skip
-    def test_bringup_a(self):
-        pass
+    def test_bringup_a_1(self):
+        results = [
+            15887950,
+            16495136,
+            527345,
+            704524,
+            1553684,
+            12683156,
+            11100544,
+            12249484,
+            7753432,
+            5908254,
+        ]
+        n = 123
+        for i in range(10):
+            n = next_number(n)
+            assert n == results[i]
 
-    @pytest.mark.skip
+    def test_bringup_a_2(self):
+        assert self.day.run_part("a", "test1") == 37327623
+
     def test_bringup_b(self):
-        pass
+        assert score([1, 2, 3, 2024], [-2, 1, -1, 3]) == 23
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 15303617151
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 1727
