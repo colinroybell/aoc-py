e17ae179ed4c738086f7a3e4e37c66dc28ad3c8f
colin <colin@ps116.org.uk>
2024-12-23 08:25:08 +0000

    2024/23

--- src/aoc2024/day23.py
+++ src/aoc2024/day23.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from collections import defaultdict
 
 
 class Run_2024_23(DayBase):
@@ -8,11 +9,53 @@ class Run_2024_23(DayBase):
 
 
 def part_a(input):
-    assert 0, "not implemented"
+    connections = defaultdict(lambda: set())
+    for line in input_generator(input):
+        c0 = line[0:2]
+        c1 = line[3:5]
+        connections[c0].add(c1)
+        connections[c1].add(c0)
+
+    count = 0
+    for c0, conn in connections.items():
+        for c1 in conn:
+            if c0 < c1:
+                intersect = conn.intersection(connections[c1])
+                for c2 in intersect:
+                    if c1 < c2:
+                        print(c0, c1, c2)
+                        if c0[0] == "t" or c1[0] == "t" or c2[0] == "t":
+                            count += 1
+    return count
+
+
+def recurse(connections, existing):
+    if existing == []:
+        new_c = connections.keys()
+    else:
+        new_c = connections[existing[0]]
+        for e in existing[1:]:
+            new_c = new_c.intersection(connections[e])
+
+    best = ",".join(existing)
+    for c in new_c:
+        if existing and c < existing[-1]:
+            continue
+        pw = recurse(connections, existing + [c])
+        if len(pw) > len(best):
+            best = pw
+    return best
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    connections = defaultdict(lambda: set())
+    for line in input_generator(input):
+        c0 = line[0:2]
+        c1 = line[3:5]
+        connections[c0].add(c1)
+        connections[c1].add(c0)
+
+    return recurse(connections, [])
 
 
 if __name__ == "__main__":
--- tests/func/aoc2024/test_day23.py
+++ tests/func/aoc2024/test_day23.py
@@ -6,18 +6,14 @@ class Test_2024_23:
     def setup_class(self):
         self.day = Run_2024_23()
 
-    @pytest.mark.skip
     def test_bringup_a(self):
-        pass
+        assert self.day.run_part("a", "test1") == 7
 
-    @pytest.mark.skip
     def test_bringup_b(self):
-        pass
+        assert self.day.run_part("b", "test1") == "co,de,ka,ta"
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 1253
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == "ag,bt,cq,da,hp,hs,mi,pa,qd,qe,qi,ri,uq"
