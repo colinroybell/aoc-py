9a6bfe3432dec1c812f818ce3c6112a96a91affb
colin <colin@ps116.org.uk>
2024-12-25 08:58:52 +0000

    2024/25

--- src/aoc2024/day25.py
+++ src/aoc2024/day25.py
@@ -8,7 +8,41 @@ class Run_2024_25(DayBase):
 
 
 def part_a(input):
-    assert 0, "not implemented"
+    lines = []
+    keys = []
+    locks = []
+    for line_count, line in enumerate(input_generator(input)):
+        if line:
+            lines.append(line)
+        print(line_count,lines)
+        if line_count%8 == 6:
+            c = lines[0][0]
+            lens = []
+            for col in range(5):
+                r= 0
+                while lines[r][col] == c:
+                    r += 1
+                if c == '#':
+                    lens.append(r-1)
+                else:
+                    lens.append(6-r)
+            if c == '#':
+                locks.append(lens)
+            else:
+                keys.append(lens)
+            lines = []            
+    print(locks)
+    print(keys)                                
+
+    total = 0
+    for lock in locks:
+        for key in keys:
+            overlap = max([lock[col] + key[col] -5 for col in range(5)])
+            if overlap <=0:
+                total += 1
+    return total            
+        
+
 
 
 def part_b(input):
