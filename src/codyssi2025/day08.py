eade4e73df19268565b222cbe3dbc1477b5c2894
colin <colin@ps116.org.uk>
2025-04-21 09:45:02 +0100

    Codyssi2025/8

--- src/codyssi2025/day08.py
+++ src/codyssi2025/day08.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_2025_08(DayBase):
@@ -8,16 +9,49 @@ class Run_2025_08(DayBase):
     PREFIX = "codyssi"
 
 
+def count_alpha(c):
+    return 1 if c.isalpha() else 0
+
+
 def part_1(input):
-    assert 0, "not implemented"
+    total = 0
+    for line in input_generator(input):
+
+        fn = count_alpha
+
+        total += sum(fn(c) for c in line)
+    return total
 
 
-def part_2(input):
-    assert 0, "not implemented"
+def isalphahyphen(c, part=2):
+    return c.isalpha() or c == "-" and part == 2
+
+
+def reduce(line, part=2):
+    out = ""
+    for c in line:
+        if len(out) > 0:
+            last = out[-1]
+            reduce = False
+            if (last.isnumeric() and isalphahyphen(c, part)) or (
+                c.isnumeric() and isalphahyphen(last, part)
+            ):
+                out = out[:-1]
+                continue
+        out += c
+    return out
+
+
+def part_2(input, part=2):
+    total = 0
+    for line in input_generator(input):
+        line = reduce(line, part)
+        total += len(line)
+    return total
 
 
 def part_3(input):
-    assert 0, "not implemented"
+    return part_2(input, part=3)
 
 
 if __name__ == "__main__":
--- tests/func/codyssi2025/test_day08.py
+++ tests/func/codyssi2025/test_day08.py
@@ -1,5 +1,5 @@
 import pytest
-from codyssi2025.day08 import Run_2025_08
+from codyssi2025.day08 import Run_2025_08, reduce
 
 
 class Test_2025_08:
@@ -7,22 +7,46 @@ class Test_2025_08:
         self.day = Run_2025_08()
 
     def test_bringup_1(self):
-        pass
+        assert self.day.run_part("1", "test1") == 52
+
+    def test_bringup_2_1(self):
+        assert reduce("baa3") == "ba"
+
+    def test_bringup_2_2(self):
+        assert reduce("321ab") == "3"
+
+    def test_bringup_2_3(self):
+        assert reduce("a7b") == "a" or reduce("a7b") == "b"
+
+    def test_bringup_2_4(self):
+        assert reduce("z-4") == "z"
 
     def test_bringup_2(self):
-        pass
+        assert self.day.run_part("2", "test1") == 18
+
+    def test_bringup_3_1(self):
+        assert reduce("baa3", part=3) == "ba"
+
+    def test_bringup_3_2(self):
+        assert reduce("321ab", part=3) == "3"
+
+    def test_bringup_3_3(self):
+        assert reduce("a7b", part=3) == "a" or reduce("a7b", part=3) == "b"
+
+    def test_bringup_3_4(self):
+        assert reduce("z-4", part=3) == "z-4"
+
+    def test_bringup_3_5(self):
+        assert reduce("a3-b6", part=3) == "-"
 
     def test_bringup_3(self):
-        pass
+        assert self.day.run_part("3", "test1") == 26
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 4685
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 800
 
-    @pytest.mark.skip
     def test_regression_3(self):
-        assert self.day.run_part("3") == 0
+        assert self.day.run_part("3") == 1400
