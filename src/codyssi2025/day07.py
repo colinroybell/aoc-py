9089c8a4e35dc5fde5b3f72b7036d0e5f80328e3
colin <colin@ps116.org.uk>
2025-04-21 08:57:34 +0100

    Codyssi2025/7

--- src/codyssi2025/day07.py
+++ src/codyssi2025/day07.py
@@ -9,15 +9,86 @@ class Run_2025_07(DayBase):
 
 
 def part_1(input):
-    assert 0, "not implemented"
+    freqs = [None]
+    generator = input_generator(input)
+    for line in generator:
+        if not line:
+            break
+        freqs.append(int(line))
+
+    for line in generator:
+        if not line:
+            break
+        fields = line.split("-")
+        p0 = int(fields[0])
+        p1 = int(fields[1])
+        freqs[p0], freqs[p1] = freqs[p1], freqs[p0]
+
+    line = next(generator)
+    pos = int(line)
+    return freqs[pos]
 
 
 def part_2(input):
-    assert 0, "not implemented"
+    freqs = [None]
+    generator = input_generator(input)
+    for line in generator:
+        if not line:
+            break
+        freqs.append(int(line))
+
+    swaps = []
+
+    for line in generator:
+        if not line:
+            break
+        fields = line.split("-")
+        p0 = int(fields[0])
+        p1 = int(fields[1])
+        swaps.append((p0, p1))
+
+    length = len(swaps)
+
+    for s in range(length):
+        p0 = swaps[s][0]
+        p1 = swaps[s][1]
+        p2 = swaps[(s + 1) % length][0]
+
+        freqs[p0], freqs[p1], freqs[p2] = freqs[p2], freqs[p0], freqs[p1]
+
+    line = next(generator)
+    pos = int(line)
+    return freqs[pos]
 
 
 def part_3(input):
-    assert 0, "not implemented"
+    freqs = [None]
+    generator = input_generator(input)
+    for line in generator:
+        if not line:
+            break
+        freqs.append(int(line))
+
+    length = len(freqs) - 1
+
+    for line in generator:
+        if not line:
+            break
+        fields = line.split("-")
+        p0 = int(fields[0])
+        p1 = int(fields[1])
+        if p0 > p1:
+            p0, p1 = p1, p0
+        base1 = p1
+
+        while p0 < base1 and p1 <= length:
+            freqs[p0], freqs[p1] = freqs[p1], freqs[p0]
+            p0 += 1
+            p1 += 1
+
+    line = next(generator)
+    pos = int(line)
+    return freqs[pos]
 
 
 if __name__ == "__main__":
--- tests/func/codyssi2025/test_day07.py
+++ tests/func/codyssi2025/test_day07.py
@@ -7,22 +7,19 @@ class Test_2025_07:
         self.day = Run_2025_07()
 
     def test_bringup_1(self):
-        pass
+        assert self.day.run_part("1", "test1") == 45
 
     def test_bringup_2(self):
-        pass
+        assert self.day.run_part("2", "test1") == 796
 
     def test_bringup_3(self):
-        pass
+        assert self.day.run_part("3", "test1") == 827
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 49406
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 44063
 
-    @pytest.mark.skip
     def test_regression_3(self):
-        assert self.day.run_part("3") == 0
+        assert self.day.run_part("3") == 21844
