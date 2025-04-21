fb5cca2e8f6139153c063f6277d5e12c0354e0c8
colin <colin@ps116.org.uk>
2025-04-21 10:28:03 +0100

    Codyssi2025/9

--- src/codyssi2025/day09.py
+++ src/codyssi2025/day09.py
@@ -8,16 +8,60 @@ class Run_2025_09(DayBase):
     PREFIX = "codyssi"
 
 
-def part_1(input):
-    assert 0, "not implemented"
+def part_1(input, part=1):
+    generator = input_generator(input)
+    balances = {}
+    debts = {}
+    for line in generator:
+        if not line:
+            break
+        fields = line.split()
+        balances[fields[0]] = int(fields[2])
+        debts[fields[0]] = []
+
+    for line in generator:
+        fields = line.split()
+        debit = fields[1]
+        credit = fields[3]
+        amount = int(fields[5])
+        if part == 2:
+            amount = min(amount, balances[debit])
+        if part == 3:
+            if amount > balances[debit]:
+                debts[debit].append([credit, amount - balances[debit]])
+                amount = balances[debit]
+
+        balances[debit] -= amount
+        balances[credit] += amount
+        if part == 3:
+            repaid = True
+            while repaid:
+                repaid = False
+                for a in balances.keys():
+                    while len(debts[a]) > 0 and balances[a] > 0:
+                        repaid = True
+                        c = debts[a][0][0]
+                        d = debts[a][0][1]
+                        payment = min(balances[a], d)
+
+                        balances[a] -= payment
+                        balances[c] += payment
+                        if payment == d:
+                            debts[a] = debts[a][1:]
+                        else:
+                            debts[a][0][1] -= payment
+
+    top3 = sorted(balances.items(), key=lambda x: x[1], reverse=True)[:3]
+
+    return sum(top3[i][1] for i in range(3))
 
 
 def part_2(input):
-    assert 0, "not implemented"
+    return part_1(input, part=2)
 
 
 def part_3(input):
-    assert 0, "not implemented"
+    return part_1(input, part=3)
 
 
 if __name__ == "__main__":
--- tests/func/codyssi2025/test_day09.py
+++ tests/func/codyssi2025/test_day09.py
@@ -7,22 +7,19 @@ class Test_2025_09:
         self.day = Run_2025_09()
 
     def test_bringup_1(self):
-        pass
+        assert self.day.run_part("1", "test1") == 2870
 
     def test_bringup_2(self):
-        pass
+        assert self.day.run_part("2", "test1") == 2542
 
     def test_bringup_3(self):
-        pass
+        assert self.day.run_part("3", "test1") == 2511
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 11197
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 5746
 
-    @pytest.mark.skip
     def test_regression_3(self):
-        assert self.day.run_part("3") == 0
+        assert self.day.run_part("3") == 8228
