831439a185b1bd6f1cbda8ab8ca2dff48aa3cfed
colin <colin@ps116.org.uk>
2025-11-09 14:56:33 +0000

    EC2025/05

--- src/ec2025/day05.py
+++ src/ec2025/day05.py
@@ -8,16 +8,86 @@ class Run_2025_05(DayBase):
     PREFIX = "ec"
 
 
+def score(line):
+    id, rest = line.split(":")
+    spine = []
+    left = []
+    right = []
+    score = ""
+    for r in rest.split(","):
+        val = int(r)
+        for p in range(len(spine)):
+            if left[p] == None and val < spine[p]:
+                left[p] = val
+                break
+            if right[p] == None and val > spine[p]:
+                right[p] = val
+                break
+        else:
+            spine.append(val)
+            left.append(None)
+            right.append(None)
+            score += r
+    return int(score)
+
+
+def sword(line):
+    id, rest = line.split(":")
+    spine = []
+    left = []
+    right = []
+    score = ""
+    for r in rest.split(","):
+        val = int(r)
+        for p in range(len(spine)):
+            if left[p] == None and val < spine[p]:
+                left[p] = val
+                break
+            if right[p] == None and val > spine[p]:
+                right[p] = val
+                break
+        else:
+            spine.append(val)
+            left.append(None)
+            right.append(None)
+            score += r
+    row_sc = ""
+    rows = []
+    for p in range(len(spine)):
+        if left[p]:
+            row_sc += str(left[p])
+        row_sc += str(spine[p])
+        if right[p]:
+            row_sc += str(right[p])
+        rows.append(int(row_sc))
+
+    return (int(id), int(score), rows)
+
+
 def part_1(input):
-    assert 0, "not implemented"
+    line = next(input_generator(input))
+    return score(line)
 
 
 def part_2(input):
-    assert 0, "not implemented"
+    scores = []
+    for line in input_generator(input):
+        scores.append(score(line))
+    return max(scores) - min(scores)
 
 
 def part_3(input):
-    assert 0, "not implemented"
+    swords = []
+    for line in input_generator(input):
+        id, score, rows = sword(line)
+        vals = [score] + rows + [id]
+        swords.append((id, vals))
+    swords.sort(key=lambda s: s[1], reverse=True)
+    checksum = 0
+    for i, s in enumerate(swords):
+        print(i, s[0], s[1][0])
+        checksum += (i + 1) * s[0]
+    return checksum
 
 
 if __name__ == "__main__":
--- tests/func/ec2025/test_day05.py
+++ tests/func/ec2025/test_day05.py
@@ -6,26 +6,23 @@ class Test_2025_05:
     def setup_class(self):
         self.day = Run_2025_05()
 
-    @pytest.mark.skip
     def test_bringup_1(self):
-        assert self.day.run_part("1", "test1") == 0
+        assert self.day.run_part("1", "test1") == 581078
 
-    @pytest.mark.skip
     def test_bringup_2(self):
-        assert self.day.run_part("2", "test2") == 0
+        assert self.day.run_part("2", "test2") == 77053
 
-    @pytest.mark.skip
-    def test_bringup_3(self):
-        assert self.day.run_part("3", "test3") == 0
+    def test_bringup_3_1(self):
+        assert self.day.run_part("3", "test3") == 260
 
-    @pytest.mark.skip
+    def test_bringup_3_2(self):
+        assert self.day.run_part("3", "test4") == 4
+
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 8762363643
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 8757643946766
 
-    @pytest.mark.skip
     def test_regression_3(self):
-        assert self.day.run_part("3") == 0
+        assert self.day.run_part("3") == 31782246
