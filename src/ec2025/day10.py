34e27223486f4187e513989ff223d90496b18894
colin <colin@ps116.org.uk>
2025-11-15 07:29:10 +0000

    EC2025/10 parts 1,2

--- src/ec2025/day10.py
+++ src/ec2025/day10.py
@@ -1,5 +1,7 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from utils.vec_2d import Vec2d
+from utils.grid_2d import Grid2d
 
 
 class Run_2025_10(DayBase):
@@ -8,12 +10,105 @@ class Run_2025_10(DayBase):
     PREFIX = "ec"
 
 
-def part_1(input):
-    assert 0, "not implemented"
+def part_1(input, steps=4):
+    grid = Grid2d()
+    width, height = grid.read_from_file_strings(input)
+
+    moves = [
+        Vec2d(2, 1),
+        Vec2d(2, -1),
+        Vec2d(1, 2),
+        Vec2d(1, -2),
+        Vec2d(-1, 2),
+        Vec2d(-1, -2),
+        Vec2d(-2, 1),
+        Vec2d(-2, -1),
+    ]
+
+    found = False
+    for x in range(width):
+        for y in range(height):
+            pos = Vec2d(x, y)
+            if grid.get(pos) == "D":
+                found = True
+                break
+        if found:
+            break
+    assert found
+
+    queue = []
+    queue.append((pos, 0))
+    done = set()
+    count = 0
+    while queue:
+        (p, s) = queue[0]
+        queue = queue[1:]
+        if p in done:
+            continue
+        done.add(p)
+        c = grid.get(p)
+        if not c:
+            # off grid
+            continue
+        if c == "S":
+            count += 1
+        if s < steps:
+            for m in moves:
+                queue.append((p + m, s + 1))
+    return count
 
 
-def part_2(input):
-    assert 0, "not implemented"
+def part_2(input, steps=20):
+    grid = Grid2d()
+    width, height = grid.read_from_file_strings(input)
+
+    moves = [
+        Vec2d(2, 1),
+        Vec2d(2, -1),
+        Vec2d(1, 2),
+        Vec2d(1, -2),
+        Vec2d(-1, 2),
+        Vec2d(-1, -2),
+        Vec2d(-2, 1),
+        Vec2d(-2, -1),
+    ]
+
+    found = False
+    for x in range(width):
+        for y in range(height):
+            pos = Vec2d(x, y)
+            if grid.get(pos) == "D":
+                found = True
+                break
+        if found:
+            break
+    assert found
+
+    queue = []
+    queue.append((pos, 0))
+    done = set()
+    sheep_done = set()
+    count = 0
+    while queue:
+        (p, s) = queue[0]
+        queue = queue[1:]
+        if (p, s) in done:
+            continue
+        c = grid.get(p)
+        if not c:
+            # off grid
+            continue
+        done.add((p, s))
+        if c != "#":
+            for d in range(s - 1, s + 1):
+                start_pos = p + Vec2d(0, -d)
+                if grid.get(start_pos) == "S" and start_pos not in sheep_done:
+                    count += 1
+                    sheep_done.add(start_pos)
+        if s < steps:
+            for m in moves:
+                queue.append((p + m, s + 1))
+    return count
 
 
 def part_3(input):
--- tests/func/ec2025/test_day10.py
+++ tests/func/ec2025/test_day10.py
@@ -6,25 +6,21 @@ class Test_2025_10:
     def setup_class(self):
         self.day = Run_2025_10()
 
-    @pytest.mark.skip
     def test_bringup_1(self):
-        assert self.day.run_part("1", "test1") == 0
+        assert self.day.run_part("1", "test1", steps=3) == 27
 
-    @pytest.mark.skip
     def test_bringup_2(self):
-        assert self.day.run_part("2", "test2") == 0
+        assert self.day.run_part("2", "test2", steps=3) == 27
 
     @pytest.mark.skip
     def test_bringup_3(self):
         assert self.day.run_part("3", "test3") == 0
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 164
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 1734
 
     @pytest.mark.skip
     def test_regression_3(self):
