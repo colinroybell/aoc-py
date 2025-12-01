17d3abf4c04fe768a778f813077dee4bd8b69a05
colin <colin@ps116.org.uk>
2025-12-01 19:21:12 +0000

    Refactored EC2025/19 part 1

--- src/ec2025/day19.py
+++ src/ec2025/day19.py
@@ -8,30 +8,42 @@ class Run_2025_19(DayBase):
     PREFIX = "ec"
 
 
+class Slit:
+    def __init__(self, x, min_y, max_y):
+        self.x = x
+        self.min_y = min_y
+        self.max_y = max_y
+
+    def project(self, other):
+        w = other.x - self.x
+        max_y = self.max_y + w
+        min_y = self.min_y - w
+
+        new_max_y = min(max_y, other.max_y)
+        if (new_max_y - other.x) % 2:
+            new_max_y -= 1
+        new_min_y = max(min_y, other.min_y)
+        if (new_min_y - other.x) % 2:
+            new_min_y += 1
+        if new_min_y <= new_max_y:
+            return Slit(other.x, new_min_y, new_max_y)
+        else:
+            return None
+
+
 def part_1(input):
     x = 0
     min_y = 0
     max_y = 0
+    current = Slit(0, 0, 0)
     for line in input_generator(input):
         (slit_x, slit_base, slit_size) = [int(n) for n in line.split(",")]
-        w = slit_x - x
-        x = slit_x
         slit_min = slit_base
         slit_max = slit_base + slit_size - 1
-        new_max_y = min(max_y + w, slit_max)
-        new_min_y = max(min_y - w, slit_min)
-        # Allow for the fact we can only go diagonally
-        if (new_max_y - x) % 2:
-            new_max_y -= 1
-        if (new_min_y - x) % 2:
-            new_min_y += 1
-
-        assert new_max_y >= min_y - w
-        assert new_min_y <= max_y + w
-
-        min_y = new_min_y
-        max_y = new_max_y
-    return (x + min_y) // 2
+        other = Slit(slit_x, slit_min, slit_max)
+        project = current.project(other)
+        current = project
+    return (current.x + current.min_y) // 2
 
 
 def part_2(input):
