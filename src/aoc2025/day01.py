738a08214d68a52457cd68e33ab9e6f84a8087cd
colin <colin@ps116.org.uk>
2025-12-01 06:59:21 +0000

    AOC2025/01

--- src/aoc2025/day01.py
+++ src/aoc2025/day01.py
@@ -7,12 +7,56 @@ class Run_2025_01(DayBase):
     DAY = "01"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def part_a(input, part="a"):
+    pos = 50
+    mul = {"R": 1, "L": -1}
+    total = 0
+    rounds = 0
+    for line in input_generator(input):
+        dir = line[0]
+        steps = int(line[1:])
+        start = pos
+        pos += mul[dir] * steps
+        pos %= 100
+        if pos == 0:
+            total += 1
+        print(dir, steps, pos, rounds, total)
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    pos = 50
+    mul = {"R": 1, "L": -1}
+    total = 0
+    rounds = 0
+    for line in input_generator(input):
+        dir = line[0]
+        steps = int(line[1:])
+        start = pos
+        pos += mul[dir] * steps
+        rounds = pos // 100
+        if rounds > 0:
+            total += rounds
+        elif rounds < 0:
+            total += abs(rounds)
+            if start == 0:
+                total -= 1
+        pos = pos % 100
+        if pos == 0 and rounds <= 0:
+            total += 1
+        print(dir, steps, pos, rounds, total)
+    return total
+
+
+def notes(input):
+    """
+    Quite awkward to get part b correct. Rightward fell out correctly,
+    but leftward we need a couple of offsets to deal with the case where
+    we are starting or ending on a zero.
+
+    Needed to write own test case 2 to sort it out - goes from 50 to 800
+    and back picking up all the coverage cases.
+    """
 
 
 if __name__ == "__main__":
--- tests/func/aoc2025/test_day01.py
+++ tests/func/aoc2025/test_day01.py
@@ -6,18 +6,18 @@ class Test_2025_01:
     def setup_class(self):
         self.day = Run_2025_01()
 
-    @pytest.mark.skip
     def test_bringup_a(self):
-        assert self.day.run_part("a", "test1") == 0
+        assert self.day.run_part("a", "test1") == 3
 
-    @pytest.mark.skip
     def test_bringup_b(self):
-        assert self.day.run_part("b", "test1") == 0
+        assert self.day.run_part("b", "test1") == 6
 
-    @pytest.mark.skip
+    def test_bringup_b_2(self):
+        # My test: goes from 50-800 rightwards (8 hits), then back to 50 leftwards (7) picking up all the combinations
+        assert self.day.run_part("b", "test2") == 15
+
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 1084
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 6475
