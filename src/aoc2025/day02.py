205becc44f9943d89a9aaa36f7331f61a7ec4618
colin <colin@ps116.org.uk>
2025-12-02 07:54:45 +0000

    AOC2025/02

--- src/aoc2025/day02.py
+++ src/aoc2025/day02.py
@@ -7,12 +7,45 @@ class Run_2025_02(DayBase):
     DAY = "02"
 
 
-def part_a(input):
-    assert 0, "not implemented"
+def is_invalid(string, part):
+    length = len(string)
+    if part == "a":
+        if length % 2:
+            return False
+        else:
+            return string[: length // 2] == string[length // 2 :]
+    else:
+        for n in range(2, length + 1):
+            if length % n == 0:
+                s = length // n
+                comp = string[:s]
+                for i in range(1, n):
+                    if string[i * s : (i + 1) * s] != comp:
+                        break
+                else:
+                    return True
+        return False
+
+
+def part_a(input, part="a"):
+    ranges = next(input_generator(input)).split(",")
+    count = 0
+    for r in ranges:
+        low, high = [int(n) for n in r.split("-")]
+        for id in range(low, high + 1):
+            if is_invalid(str(id), part):
+                print(id)
+                count += int(id)
+    return count
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    return part_a(input, part="b")
+
+
+def notes():
+    """
+    First part does a full search, whereas it ought to be possible to do it via construction. Second part ditto. Current time for part 2 is 1.5s so nice to get it down"""
 
 
 if __name__ == "__main__":
--- tests/func/aoc2025/test_day02.py
+++ tests/func/aoc2025/test_day02.py
@@ -1,23 +1,25 @@
 import pytest
-from aoc2025.day02 import Run_2025_02
+from aoc2025.day02 import Run_2025_02, is_invalid
 
 
 class Test_2025_02:
     def setup_class(self):
         self.day = Run_2025_02()
 
-    @pytest.mark.skip
     def test_bringup_a(self):
-        assert self.day.run_part("a", "test1") == 0
+        assert self.day.run_part("a", "test1") == 1227775554
 
-    @pytest.mark.skip
+    def test_bringup_invalid(self):
+        assert is_invalid("11", "b")
+        assert is_invalid("21212121", "b")
+        assert is_invalid("1010", "b")
+        assert not is_invalid("222220", "b")
+
     def test_bringup_b(self):
-        assert self.day.run_part("b", "test1") == 0
+        assert self.day.run_part("b", "test1") == 4174379265
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 34826702005
 
-    @pytest.mark.skip
     def test_regression_b(self):
-        assert self.day.run_part("b") == 0
+        assert self.day.run_part("b") == 43287141963
