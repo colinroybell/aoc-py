53c3635ecb6d4a64b5338b1eaba6b0dac762eab3
colin <colin@ps116.org.uk>
2025-12-03 19:58:50 +0000

    AOC2025/02

--- src/aoc2025/day03.py
+++ src/aoc2025/day03.py
@@ -7,12 +7,65 @@ class Run_2025_03(DayBase):
     DAY = "03"
 
 
+def joltage(string):
+    batteries = [int(n) for n in string]
+    max = 0
+    max_pos = 0
+    for i in range(len(batteries) - 1):
+        if batteries[i] > max:
+            max = batteries[i]
+            max_pos = i
+    max2 = 0
+    for i in range(max_pos + 1, len(batteries)):
+        if batteries[i] > max2:
+            max2 = batteries[i]
+    return max * 10 + max2
+
+
+def joltageN(string, N):
+    lists = [[] for n in range(10)]
+    batteries = [int(n) for n in string]
+    for i in range(len(batteries)):
+        lists[batteries[i]].append(i)
+
+    count = len(batteries)
+    pos = 0
+    value = 0
+    for n in range(N):
+        found = False
+        search = 9
+        while not found:
+            # Cull anything which is left of the position
+            cull = 0
+            for i, p in enumerate(lists[search]):
+                if p < pos:
+                    cull += 1
+                if p >= pos and p < count - N + n + 1:
+                    value = value * 10 + search
+                    # Cull anything left of the new position
+                    lists[search] = lists[search][i + 1 :]
+                    found = True
+                    pos = p
+                    break
+            if not found:
+                if cull:
+                    lists[search] = lists[search][cull:]
+                search -= 1
+    return value
+
+
 def part_a(input):
-    assert 0, "not implemented"
+    total = 0
+    for line in input_generator(input):
+        total += joltage(line)
+    return total
 
 
 def part_b(input):
-    assert 0, "not implemented"
+    total = 0
+    for line in input_generator(input):
+        total += joltageN(line, 12)
+    return total
 
 
 if __name__ == "__main__":
--- tests/func/aoc2025/test_day03.py
+++ tests/func/aoc2025/test_day03.py
@@ -1,22 +1,35 @@
 import pytest
-from aoc2025.day03 import Run_2025_03
+from aoc2025.day03 import Run_2025_03, joltage, joltageN
 
 
 class Test_2025_03:
     def setup_class(self):
         self.day = Run_2025_03()
 
-    @pytest.mark.skip
+    def test_joltage(self):
+        assert joltage("987654321111111") == 98
+        assert joltage("811111111111119") == 89
+        assert joltage("234234234234278") == 78
+        assert joltage("818181911112111") == 92
+
+    def test_joltageN(self):
+        assert joltageN("987654321111111", 2) == 98
+        assert joltageN("811111111111119", 2) == 89
+        assert joltageN("234234234234278", 2) == 78
+        assert joltageN("818181911112111", 2) == 92
+        assert joltageN("987654321111111", 12) == 987654321111
+        assert joltageN("811111111111119", 12) == 811111111119
+        assert joltageN("234234234234278", 12) == 434234234278
+        assert joltageN("818181911112111", 12) == 888911112111
+
     def test_bringup_a(self):
-        assert self.day.run_part("a", "test1") == 0
+        assert self.day.run_part("a", "test1") == 357
 
-    @pytest.mark.skip
     def test_bringup_b(self):
-        assert self.day.run_part("b", "test1") == 0
+        assert self.day.run_part("b", "test1") == 3121910778619
 
-    @pytest.mark.skip
     def test_regression_a(self):
-        assert self.day.run_part("a") == 0
+        assert self.day.run_part("a") == 17766
 
     @pytest.mark.skip
     def test_regression_b(self):
