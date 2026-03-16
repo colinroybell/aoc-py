a370ce2c9fecba640ef598de3a8880a1aab70c6d
colin <colin@ps116.org.uk>
2026-03-16 15:21:48 +0000

    ECST02/02 parts 1 and 2

--- src/ecst02/day02.py
+++ src/ecst02/day02.py
@@ -7,17 +7,49 @@ class Run_st02_02(DayBase):
     DAY = "02"
     PREFIX = "ec"
 
+    NOTES = """
+    For part 3 we need at least a more efficient data structure that doesn't involve massive string 
+    concatenation. I can't see any sensible patterns.
+    """
+
 
 def part_1(input):
-    assert 0, "not implemented"
+    # Add an X to simplify edge conditions
+    balloons = next(input_generator(input)) + "X"
+    shots = "RGB"
+    count = 0
+    while 1:
+        for shot in shots:
+            count += 1
+            while balloons[0] == shot:
+                balloons = balloons[1:]
+            balloons = balloons[1:]
+            # If last balloon matched shot, we will also pop the X.
+            if balloons == "" or balloons == "X":
+                return count
 
 
-def part_2(input):
-    assert 0, "not implemented"
+def part_2(input, repeats=100):
+    pattern = next(input_generator(input))
+    balloons = pattern * repeats
+    count = 0
+    shots = "RGB"
+    while balloons:
+        shot = shots[count % 3]
+        count += 1
+        if count % 1000 == 0:
+            print(count, len(balloons))
+        length = len(balloons)
+        if length % 2 == 0 and balloons[0] == shot:
+            balloons = balloons[: length // 2] + balloons[length // 2 + 1 :]
+        balloons = balloons[1:]
+    return count
 
 
 def part_3(input):
-    assert 0, "not implemented"
+    for n in [1, 10, 100, 1000, 10000]:
+        print("answer", n, part_2(input, repeats=n))
+    return part_2(input, repeats=100000)
 
 
 if __name__ == "__main__":
--- tests/func/ecst02/test_day02.py
+++ tests/func/ecst02/test_day02.py
@@ -6,25 +6,30 @@ class Test_st02_02:
     def setup_class(self):
         self.day = Run_st02_02()
 
-    @pytest.mark.skip
     def test_bringup_1(self):
-        assert self.day.run_part("1", "test1") == 0
+        assert self.day.run_part("1", "test1") == 7
 
-    @pytest.mark.skip
-    def test_bringup_2(self):
-        assert self.day.run_part("2", "test2") == 0
+    def test_bringup_2_1(self):
+        assert self.day.run_part("2", "test2", repeats=5) == 14
+
+    def test_bringup_2_2(self):
+        assert self.day.run_part("2", "test3", repeats=10) == 304
+
+    def test_bringup_2_3(self):
+        assert self.day.run_part("2", "test3", repeats=50) == 1464
+
+    def test_bringup_2_4(self):
+        assert self.day.run_part("2", "test3", repeats=100) == 2955
 
     @pytest.mark.skip
     def test_bringup_3(self):
         assert self.day.run_part("3", "test3") == 0
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 119
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == 21581
 
     @pytest.mark.skip
     def test_regression_3(self):
