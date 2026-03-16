b901e9430202f2bba1e7f0409719a3cc79ed1633
colin <colin@ps116.org.uk>
2026-03-16 10:43:23 +0000

    ECST02/01 parts 1,2

--- src/ecst02/day01.py
+++ src/ecst02/day01.py
@@ -1,5 +1,7 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+from utils.grid_2d import Grid2d
+from utils.vec_2d import Vec2d
 
 
 class Run_st02_01(DayBase):
@@ -8,15 +10,101 @@ class Run_st02_01(DayBase):
     PREFIX = "ec"
 
 
-def part_1(input):
-    assert 0, "not implemented"
+def process_run(grid, inst, slot, trace):
+    if trace:
+        assert trace[0] == inst
+        assert trace[1] == slot
+
+    pos = Vec2d(slot * 2 - 2, 0)
+    instpos = 0
+    while 1:
+        c = grid.get(pos)
+        if c == None:
+            break
+        elif c == "*":
+            dir = inst[instpos]
+            instpos += 1
+            if dir == "R":
+                adj = Vec2d(1, 0)
+            else:
+                adj = Vec2d(-1, 0)
+            if grid.get(pos + adj) == ".":
+                pos = pos + adj
+            else:
+                # bounce off the wall
+                pos = pos - adj
+        else:
+            pos = pos + Vec2d(0, 1)
+
+    assert pos.x % 2 == 0
+    end_slot = pos.x // 2 + 1
+    score = max(end_slot * 2 - slot, 0)
+    if trace:
+        assert trace[2] == end_slot
+        assert trace[3] == score
+    return score
 
 
-def part_2(input):
-    assert 0, "not implemented"
+def part_1(input, part=1, **kwargs):
+    generator = input_generator(input)
+    grid = Grid2d()
+    (width, height) = grid.read_from_file_strings_generator(
+        generator, stop_at_blank=True
+    )
+
+    insts = [line for line in generator]
+    # Modification to get test1 to choose a slot
+    if insts[0].startswith("slot"):
+        fields = insts[0].split()
+        first_slot = int(fields[1])
+        insts = insts[1:]
+    else:
+        first_slot = 1
+
+    traces = None
+    if "trace" in kwargs:
+        trace_generator = input_generator(kwargs["trace"])
+        _ = next(trace_generator)
+        traces = []
+        for line in trace_generator:
+            if not line:
+                break
+            fields = line.split()
+            traces.append([fields[0], int(fields[1]), int(fields[2]), int(fields[3])])
+        assert len(traces) == len(insts)
+
+    total = 0
+    if part == 1:
+        for run in range(len(insts)):
+            trace = None
+            if traces:
+                trace = traces[run]
+            total += process_run(grid, insts[run], run + first_slot, trace)
+    else:
+        slots = (width + 1) // 2
+        for run in range(len(insts)):
+            best_slot = None
+            best_score = 0
+            for slot in range(1, slots + 1):
+                score = process_run(grid, insts[run], slot, None)
+                if score > best_score:
+                    best_slot = slot
+                    best_score = score
+            if traces:
+
+                trace = traces[run]
+                assert best_slot == trace[1]
+                assert best_score == trace[3]
+            total += best_score
+
+    return total
 
 
-def part_3(input):
+def part_2(input, **kwargs):
+    return part_1(input, part=2, **kwargs)
+
+
+def part_3(input, **kwargs):
     assert 0, "not implemented"
 
 
--- tests/func/ecst02/test_day01.py
+++ tests/func/ecst02/test_day01.py
@@ -6,21 +6,21 @@ class Test_st02_01:
     def setup_class(self):
         self.day = Run_st02_01()
 
-    @pytest.mark.skip
-    def test_bringup_1(self):
-        assert self.day.run_part("1", "test1") == 0
+    def test_bringup_1_1(self):
+        assert self.day.run_part("1", "test1") == 3
 
-    @pytest.mark.skip
+    def test_bringup_1_2(self):
+        assert self.day.run_part("1", "test2", trace="part1_trace1") == 26
+
     def test_bringup_2(self):
-        assert self.day.run_part("2", "test2") == 0
+        assert self.day.run_part("2", "test3", trace="part2_trace1") == 115
 
     @pytest.mark.skip
     def test_bringup_3(self):
         assert self.day.run_part("3", "test3") == 0
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 48
 
     @pytest.mark.skip
     def test_regression_2(self):
