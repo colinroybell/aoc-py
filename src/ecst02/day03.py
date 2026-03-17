98b2dfae1070f433dccb19754f4400ac6725cdee
colin <colin@ps116.org.uk>
2026-03-17 14:21:12 +0000

    ECST02/03 parts 1 and 2

--- src/ecst02/day03.py
+++ src/ecst02/day03.py
@@ -1,5 +1,6 @@
 from utils.day_base import DayBase
 from utils.data_input import input_generator
+import re
 
 
 class Run_st02_03(DayBase):
@@ -8,12 +9,118 @@ class Run_st02_03(DayBase):
     PREFIX = "ec"
 
 
-def part_1(input):
-    assert 0, "not implemented"
+class Die:
+    def __init__(self, id, faces, seed):
+        self.id = id
+        self.seed = seed
+        self.pulse = seed
+        self.faces = faces
+        self.roll_number = 0
+        self.current = 0
+
+    def roll(self):
+        self.roll_number += 1
+        spin = self.roll_number * self.pulse
+        self.current = (self.current + spin) % len(self.faces)
+        self.pulse += spin
+        self.pulse %= self.seed
+        self.pulse += 1 + self.roll_number + self.seed
+        print("spin", spin)
+        print("self.current", self.current)
+        print("self.faces", self.faces, len(self.faces))
+        return self.faces[self.current]
+
+
+def die_from_string(line):
+    parse_re = re.compile(r"(\d+):\sfaces=\[(.*)\]\sseed=(\d+)")
+    m = parse_re.match(line)
+    assert m
+    id = int(m.group(1))
+    faces = [int(d) for d in m.group(2).split(",")]
+    seed = int(m.group(3))
+
+    die = Die(id, faces, seed)
+    return die
+
+
+def roll_test_function(input, **kwargs):
+
+    die = die_from_string(next(input_generator(input)))
+
+    assert "trace" in kwargs
+    rolls = 0
+    for line in input_generator(kwargs["trace"]):
+        if not line:
+            continue
+        if line[0] != " ":
+            continue
+        fields = line.split()
+        if fields[0] == "-":
+            continue
+        roll = int(fields[0])
+        # Could expose spin but unnecessary
+        exp_result = int(fields[2])
+        exp_pulse = int(fields[3])
+        result = None
+        while rolls < roll:
+            result = die.roll()
+            rolls += 1
+        print(result, exp_result)
+        assert result == exp_result
+        assert die.pulse == exp_pulse
+
+    return True
+
+
+def part_1(input, roll_test=False, **kwargs):
+    if roll_test:
+        return roll_test_function(input, **kwargs)
+    dice = []
+    for line in input_generator(input):
+        dice.append(die_from_string(line))
+
+    total = 0
+    roll = 0
+    while total < 10000:
+        roll += 1
+        score = 0
+        for die in dice:
+            score += die.roll()
+        total += score
+    return roll
 
 
 def part_2(input):
-    assert 0, "not implemented"
+    dice = []
+    generator = input_generator(input)
+    for line in generator:
+        if not line:
+            break
+        else:
+            dice.append(die_from_string(line))
+    track = next(generator)
+    track = [int(t) for t in track]
+    len_track = len(track)
+    players = len(dice)
+    pos = [0 for _ in range(players)]
+    done = [False for _ in range(players)]
+    num_done = 0
+    finishing_order = ""
+
+    roll = 0
+    while num_done < players:
+        roll += 1
+        for i, die in enumerate(dice):
+            if done[i]:
+                continue
+            result = die.roll()
+            if result == track[pos[i]]:
+                pos[i] += 1
+                if pos[i] == len(track):
+                    done[i] = True
+                    num_done += 1
+                    finishing_order += ",{}".format(die.id)
+    return finishing_order[1:]
 
 
 def part_3(input):
--- tests/func/ecst02/test_day03.py
+++ tests/func/ecst02/test_day03.py
@@ -6,25 +6,24 @@ class Test_st02_03:
     def setup_class(self):
         self.day = Run_st02_03()
 
-    @pytest.mark.skip
-    def test_bringup_1(self):
-        assert self.day.run_part("1", "test1") == 0
+    def test_bringup_1_1(self):
+        assert self.day.run_part("1", "test1", trace="part1_trace1", roll_test=True)
 
-    @pytest.mark.skip
+    def test_bringup_1_2(self):
+        assert self.day.run_part("1", "test2", trace="part1_trace2") == 844
+
     def test_bringup_2(self):
-        assert self.day.run_part("2", "test2") == 0
+        assert self.day.run_part("2", "test3") == "1,3,4,2"
 
     @pytest.mark.skip
     def test_bringup_3(self):
         assert self.day.run_part("3", "test3") == 0
 
-    @pytest.mark.skip
     def test_regression_1(self):
-        assert self.day.run_part("1") == 0
+        assert self.day.run_part("1") == 633
 
-    @pytest.mark.skip
     def test_regression_2(self):
-        assert self.day.run_part("2") == 0
+        assert self.day.run_part("2") == "4,7,9,8,1,5,3,2,6"
 
     @pytest.mark.skip
     def test_regression_3(self):
